54bc29299dc7c43af70233e60da6a490d8e0ed7d
Jasmine Princy Lobo <jasminelobo49@gmail.com>
2018-07-17 15:43:31 +0530

    Add files via upload

---
+++ j2.java
@@ -0,0 +1,14 @@
+class j2
+{
+public static void main(String args[])
+{
+
+System.out.println(Math.floor(-4.7));
+System.out.println(Math.ceil(-4.7));
+System.out.println(Math.round(-4.7));
+//System.out.println(Math.Min(-4.7));
+
+
+}
+}
+
